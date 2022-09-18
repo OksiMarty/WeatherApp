6e5aceaa0e05f9f078c34ab5ddd34fe9c9b34b0c
OksiMarty <hrechkasweets@gmail.com>
2022-09-18 10:36:53 +0300

    time set

--- src/app.js
+++ src/app.js
@@ -1,3 +1,42 @@
+function formatDate(time) {
+  let date = new Date(time);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let day = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let dayofweek = day[date.getDay()];
+  let month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let monthofyear = month[date.getMonth()];
+  let year = date.getFullYear();
+  return `${hours}:${minutes} ${dayofweek} ${monthofyear} ${year}`;
+}
+
 function showWeather(response) {
   document.querySelector("#main-city").innerHTML = response.data.name;
   document.querySelector("#main-temperature").innerHTML = Math.round(
@@ -10,8 +49,12 @@ function showWeather(response) {
   );
   document.querySelector("#main-humidity").innerHTML =
     response.data.main.humidity;
+  document.querySelector("#date").innerHTML = formatDate(
+    response.data.dt * 1000
+  );
 }
 
 let key = "27218fb510ea3727370c3caaa80041fc";
-let url = `https://api.openweathermap.org/data/2.5/weather?q=Oslo&appid=${key}&units=metric`;
+//let city = "Los Angeles";
+let url = `https://api.openweathermap.org/data/2.5/weather?q=Los Angeles&appid=${key}&units=metric`;
 axios.get(url).then(showWeather);
